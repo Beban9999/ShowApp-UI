7f362701bde4e9664583eb6351705adcdf974fd7
Beban9999 <aleksa.aleksic99@gmail.com>
2023-12-22 17:40:06 +0100

    Migrated login to new api

--- src/stores/auth_store.ts
+++ src/stores/auth_store.ts
@@ -28,66 +28,69 @@ export const useAuthenticationStore = defineStore("AuthenticationStore", {
       if (!this.loggedIn) {
         router.push({ path: '/' })
       }
-    },
-    
-    async doLogin(username: string, password: string) {
-      try {
-        const apiUrl = 'https://localhost:7201/api/Login';
-        const requestBody = {
-          loginName: username,
-          password: password,
-        };
+    },    
+    // async doLogin(username: string, password: string) {
+    //   try {
+    //     const apiUrl = 'https://localhost:7201/api/Login';
+    //     const requestBody = {
+    //       loginName: username,
+    //       password: password,
+    //     };
 
-        const response = await fetch(apiUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestBody),
-        });
+    //     const response = await fetch(apiUrl, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(requestBody),
+    //     });
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data); // The result from the API
-          return data;
-        } else {
-          console.error('Authentication failed. Check your credentials.');
-        }
-      } catch (error) {
-        console.error('An error occurred while authenticating:', error);
-      }
-    },
-    async doRegister(username: string, password: string, fname: string, lname: string, email: string) {
-      try {
-        const apiUrl = 'https://localhost:7201/api/Login/register';
-        const requestBody = {
-          loginName: username,
-          password: password,
-          firstName: fname,
-          lastName: lname,
-          email: email
-        }
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //       console.log(data); // The result from the API
+    //       return data;
+    //     } else {
+    //       console.error('Authentication failed. Check your credentials.');
+    //     }
+    //   } catch (error) {
+    //     console.error('An error occurred while authenticating:', error);
+    //   }
+    // },
+    // async doRegister(username: string, password: string, fname: string, lname: string, email: string) {
+    //   try {
+    //     const apiUrl = 'https://localhost:7201/api/Login/register';
+    //     const requestBody = {
+    //       loginName: username,
+    //       password: password,
+    //       firstName: fname,
+    //       lastName: lname,
+    //       email: email
+    //     }
 
-        const response = await fetch(apiUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestBody),
-        });
+    //     const response = await fetch(apiUrl, {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(requestBody),
+    //     });
 
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data); // The result from the API
-          return data;
-        } else {
-          console.error('Authentication failed. Check your credentials.');
-        }
-      } catch (error) {
-        console.error('An error occurred while authenticating:', error);
-      }
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //       console.log(data); // The result from the API
+    //       return data;
+    //     } else {
+    //       console.error('Authentication failed. Check your credentials.');
+    //     }
+    //   } catch (error) {
+    //     console.error('An error occurred while authenticating:', error);
+    //   }
+    // }
+  },
+  getters: {
+    getFullName() : string{
+      return this.userData.FirstName + " " + this.userData.LastName
     }
-
   },
   persist: {
     storage: sessionStorage
